f45e448d60d0488f6f9b2daf14092d8b5d3c6514
delmashajan <delmashajan@gmail.com>
2022-11-28 16:11:05 +0530

    read value

--- src/app/add-product/add-product.component.ts
+++ src/app/add-product/add-product.component.ts
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AddProductComponent {
 
+  title=""
+  description=""
+  price=""
+  image=""
+  sellername=""
+  quantity=""
+
+  readValue=()=>
+  {
+    let data:any ={
+      "title":this.title,"description":this.description,"price":this.price,"image":this.image,"sellername":this.sellername,"quantity":this.quantity
+    }
+    console.log(data)
+  }
+
 }
