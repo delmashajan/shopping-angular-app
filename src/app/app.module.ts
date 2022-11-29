bdaf8083cdfca51647e02a55446b7c679522194b
delmashajan <delmashajan@gmail.com>
2022-11-29 14:49:23 +0530

    services added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { RouterModule, Routes } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { ViewNewsComponent } from './view-news/view-news.component';
+import { HttpClientModule } from '@angular/common/http'
 const myRoute:Routes=[
   {
     path:"",
@@ -37,7 +38,8 @@ const myRoute:Routes=[
     BrowserModule,
     AppRoutingModule,
     RouterModule.forRoot(myRoute),
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
